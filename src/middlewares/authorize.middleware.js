7d2337561e592b7f53818f2c95648d0d77e00ddd
Javed Ansari <Javedansari2k09@gmail.com>
2026-02-01 10:26:48 +0530

    fix: correct response method in authorization middleware to use json

--- src/middlewares/authorize.middleware.js
+++ src/middlewares/authorize.middleware.js
@@ -2,7 +2,7 @@ import { ApiError } from "../utils/apiError.js";
 const authorize = (...roles) =>
   (req, res, next) => {
     if (!roles.includes(req?.user.role)) {
-      return res.status(403).res(
+      return res.status(403).json(
         new ApiError(403, "Access denied")
       )
       
